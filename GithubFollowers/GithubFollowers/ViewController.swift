ac47ea2617641455c502ded36b249930c2c66a0e
Wagner Rodrigues <wagnersr92@gmail.com>
2023-03-12 02:43:12 -0300

    commit inicial. config viewcode, add icones

---
+++ GithubFollowers/GithubFollowers/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GithubFollowers
+//
+//  Created by Wagner Rodrigues on 12/03/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemPink
+    }
+
+
+}
+
