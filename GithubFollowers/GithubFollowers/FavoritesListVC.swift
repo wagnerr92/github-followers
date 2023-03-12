8bfda5a832f18b1db7efaeb0d1cce4f3ac4e93df
Wagner Rodrigues <wagnersr92@gmail.com>
2023-03-12 03:21:52 -0300

    Criando Tabbar e NavigationController

---
+++ GithubFollowers/GithubFollowers/FavoritesListVC.swift
@@ -0,0 +1,18 @@
+//
+//  FavoriteListVC.swift
+//  GithubFollowers
+//
+//  Created by Wagner Rodrigues on 12/03/23.
+//
+
+import UIKit
+
+class FavoritesListVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .systemBlue
+    }
+    
+}
---
+++ GithubFollowers/GithubFollowers/SearchVC.swift
@@ -0,0 +1,20 @@
+//
+//  SearchVC.swift
+//  GithubFollowers
+//
+//  Created by Wagner Rodrigues on 12/03/23.
+//
+
+import UIKit
+
+class SearchVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .systemPink
+        
+    }
+
+
+}
