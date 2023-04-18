4c286b4e353b71af2290d48bd3905679b920c4ab
Wagner Rodrigues <wagnersr92@gmail.com>
2023-04-17 21:06:16 -0300

    UICollectionView Follower Cell

---
+++ GithubFollowers/GithubFollowers/CustomViews/GFAvatarImageView.swift
@@ -0,0 +1,31 @@
+//
+//  GFAvatarImageView.swift
+//  GithubFollowers
+//
+//  Created by Wagner Rodrigues on 17/04/23.
+//
+
+import UIKit
+
+class GFAvatarImageView: UIImageView {
+    
+    let placeholderImage = UIImage(named: "avatar-placeholder")
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        configure()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func configure(){
+        layer.cornerRadius = 10
+        clipsToBounds = true
+        image = placeholderImage
+        translatesAutoresizingMaskIntoConstraints = false
+        
+    }
+
+}
